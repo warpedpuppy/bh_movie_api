b0e2638f9dacca1b485fbdc464490a501ddfaa47
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-08-18 19:24:59 -0400

    typo errors

--- passport.js
+++ passport.js
@@ -1,9 +1,9 @@
 const passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy,
   Models = require('./models.js'),
-  passportJWT = require('passport-jwt');
+  passportJWT = require('passport-jwt'),
 
-let Users = Models.Users,
+  Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
