83e8ad8b191d9059142f88a7438638b0bc327d5f
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-08-19 07:25:46 -0400

    changed listening port

--- index.js
+++ index.js
@@ -271,9 +271,9 @@ app.get('/', (req, res) => {
         res.status(500).send('Something broke!');
     });
 
-    
-    app.listen(8080, () => {
-        console.log('Your app is listening on port 8080.');
+    const port = process.env.PORT || 8080;
+    app.listen(port, '0.0.0.0',() => {
+        console.log('Listening on Port ' + port);
     });
 
     
