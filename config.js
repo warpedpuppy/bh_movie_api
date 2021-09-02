05c0e202e957d2d1f8ab9c94ca5dee1b47566375
ted <ted@warpedpuppy.com>
2021-09-02 18:02:18 -0400

    added file

---
+++ config.js
@@ -0,0 +1,4 @@
+const config = {
+    CONNECTION_URI: process.env.CONNECTION_URI || 'mongodb://localhost:27017/myFlixDB'
+}
+module.exports = config;
--- index.js
+++ index.js
@@ -13,10 +13,10 @@ const { check, validationResult } = require('express-validator');
   Users = Models.User;
  
 
- // mongoose.connect('mongodb://localhost:27017/myFlixDB', {
-   // useNewUrlParser: true,
-  //  useUnifiedTopology: true 
-  //});
+//  mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+//    useNewUrlParser: true,
+//    useUnifiedTopology: true 
+//   });
 
   mongoose.connect( process.env.CONNECTION_URI, 
   { useNewUrlParser: true, useUnifiedTopology: true });
